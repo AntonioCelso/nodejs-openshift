fd72611f7752bd90e9612f1e489e4c84c9c38216
Antonio_Celso <atractorz@gmail.com>
2018-06-08 15:24:06 -0300

    Instalando o mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+	mongoose = require('mongoose');
     
 Object.assign=require('object-assign')
 
